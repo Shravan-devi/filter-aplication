f308cb495248297ce4f9da86caafdd0d564afc32
Shravan-devi <83762735+Shravan-devi@users.noreply.github.com>
2023-03-31 17:42:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,30 @@ function preload(){
 function setup(){
     canvas= createCanvas(300,300);
     canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', getPose);
+
 }
 
 function draw(){
+    image(video,0,0,300,300);
+
 }
 
 function take_snapshot(){
     save("snap.png");
+}
+
+function modelLoaded(){
+    console.log('modelLoaded');
+}
+function getPose(results){
+    if(results.length>0){
+        console.log(results);
+        console.log('nose_x'+ results[0].pose.nose.x);
+        console.log('nose_y' +results[0].pose.nose.y);
+    }
 }
